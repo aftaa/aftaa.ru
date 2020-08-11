9f118936d091e048127bd6456b62e72bc4d999c7
Maxim Gabidullin <after@ya.ru>
2020-08-11 16:56:07 +0300

    cleaned

--- migration/alter_link_block.sql
+++
@@ -1,2 +0,0 @@
-alter table link_block
-    add column private bool not null default true after name;
--- migration/top_query.sql
+++
@@ -1,5 +0,0 @@
-SELECT count(link_id) as cnt, l.id, l.name FROM link_view lv	join link l
-on l.id=link_id
-group by l.id
-order by cnt desc
-limit 10
